84f3e425c7a28254dfedcbfcaa859dadb269c859
Grace Chung <gcw.chung@gmail.com>
2020-05-02 09:09:16 -0700

    Implemented progress bar

--- static/js/videoPlayer.js
+++ static/js/videoPlayer.js
@@ -4,10 +4,11 @@ const player = document.querySelector('.player');  // parent
 
 const video = player.querySelector('.player__video'); // child
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('progress__filled');
+const progressBar = player.querySelector('.progress__filled');
 const playButton = player.querySelector('.play');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
 
 // 2. Functions
 function playVideo() {
@@ -37,21 +38,43 @@ function updateButton() {
 }
 
 function skip() {
-    console.log(this); // skip button 
-    console.log(this.dataset); // returns dataset attribute
-    console.log(this.dataset.skip); // dataset attribute value string --> 25
-    console.log(video.currentTime);
+    // console.log(this); // skip button 
+    // console.log(this.dataset); // returns dataset attribute
+    // console.log(this.dataset.skip); // dataset attribute value string --> 25
+    // console.log(video.currentTime);
+
     // adding seconds to video's current time
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function updateRange() {
+    console.log(this.name); // playback rate or volume
+    console.log(this.value); // range value
+    // video[element attribute name] = this.value;
+    video[this.name] = this.value;
+}
+
+function updateProgressBar() {
+    //  calculate percentage of video with video properties (currentTime and duration)
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
 
 
 // 3. Event Listeners
 video.addEventListener('click', playVideo);
 playButton.addEventListener('click', playVideo);
+
 // play button
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
+// progress bar
+video.addEventListener('timeupdate', updateProgressBar);
+
 // for all skip buttons
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+// for all range buttons
+ranges.forEach(range => range.addEventListener('change', updateRange));
+
