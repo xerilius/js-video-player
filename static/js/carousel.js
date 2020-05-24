62f987e9b3c04cc1fd895ac89e60cf2479c6fce4
Grace Chung <gcw.chung@gmail.com>
2020-05-24 03:16:37 -0700

    Implemented working dot nav

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -2,13 +2,15 @@
 
 const prevButton = document.querySelector(".prev-slide");
 const nextButton = document.querySelector(".next-slide");
-
 prevButton.addEventListener('click', moveSlide);
 nextButton.addEventListener('click', moveSlide);   
 
+const dots = document.querySelectorAll(".dot");
+dots.forEach(dot => dot.addEventListener('click', currentSlide));
+
+
 // Show first slide Initially
 let slideIndex = 1;
-
 showSlide(slideIndex);
 
 
@@ -16,36 +18,45 @@ showSlide(slideIndex);
 function moveSlide() {
     let slideIncrement = parseFloat(this.dataset.move);
     showSlide(slideIndex += slideIncrement);
-    console.log(slideIndex);
+}
+
+// Nav Dots
+function currentSlide() {
+    let num = parseFloat(this.dataset.slide);
+    showSlide(slideIndex = num);
 }
 
 // Function that displays carousel image
 function showSlide(imgNum) {
     const slides = document.getElementsByClassName("carousel-item");
     const dots = document.getElementsByClassName("dot");
-    
+ 
     // Hide all images in carousel
     for (let i = 0; i < slides.length; i += 1) {
         slides[i].style.display = "none";
     }
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
 
+    // if img# is (5) set reset carousel and set it to first image
     if (imgNum > slides.length) {
         slideIndex = 1;
         slides[0].style.display= "flex";
-    }
+    } 
 
-    if (imgNum < 1) {
+    //  if  img# is (-1) set image to last image
+    else if (imgNum < 1) {
         slideIndex = slides.length;
         slides[(slides.length) -1].style.display = "flex";
-    }
+    } 
 
     else {
         slides[slideIndex-1].style.display= "flex";
     }
 
+    // Dots
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
     dots[slideIndex-1].className += " active";
 }
 
